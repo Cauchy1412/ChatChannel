cf1cd681610aaa5f0e4bce546b20ee3e260e6e24
xuyuanfan <120812312@qq.com>
2021-11-03 00:55:20 +0800

    update

--- backend/chat/apis/views.py
+++ backend/chat/apis/views.py
@@ -3,8 +3,14 @@ from collections import OrderedDict
 from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-
+import json
 import pymysql
+
+MyPassWord=''
+
+with open('apis/password.json','r') as f:
+	temp = json.loads(f.read())
+	MyPassWord=temp['password']
 
 # MySQL语句 插入数据行，即注册行为
 register_sql = 'insert into user(name,password) values(%s,%s)'
@@ -36,7 +42,7 @@ add_comment_sql = 'insert into comment(user_name,dynamicId,content,gen_time) val
 def api_login(request):
 	user_name = request.data.get("username")
 	pass_word = request.data.get("password")
-	db = pymysql.connect(host="127.0.0.1",port=3306,user="root",password="011119",database="tempdb")
+	db = pymysql.connect(host="127.0.0.1",port=3306,user="root",password=MyPassWord,database="tempdb")
 	cursor = db.cursor()
 	res = cursor.execute(login_sql,(user_name,pass_word))
 	db.commit()
@@ -52,7 +58,7 @@ def api_login(request):
 def api_register(request):
 	user_name = request.data.get("username")
 	pass_word = request.data.get("password")
-	db = pymysql.connect(host="127.0.0.1",port=3306,user="root",password="011119",database="tempdb")
+	db = pymysql.connect(host="127.0.0.1",port=3306,user="root",password=MyPassWord,database="tempdb")
 	cursor = db.cursor()
 	res = cursor.execute(check_sql,(user_name))
 	db.commit()
@@ -68,7 +74,7 @@ def api_register(request):
 @api_view(['GET', 'POST'])
 def history_msg(request):
 	if request.method == 'GET':
-		db = pymysql.connect(host="127.0.0.1",port=3306,user="root",password="011119",database="tempdb")
+		db = pymysql.connect(host="127.0.0.1",port=3306,user="root",password=MyPassWord,database="tempdb")
 		cursor = db.cursor()
 		res = cursor.execute(query_sql)
 		res = cursor.fetchall()
@@ -80,7 +86,7 @@ def history_msg(request):
 		gentime = request.data.get('gentime')
 		gentime = gentime.replace('T',' ')
 		gentime = gentime.replace('Z','000')
-		db = pymysql.connect(host="127.0.0.1",port=3306,user="root",password="011119",database="tempdb")
+		db = pymysql.connect(host="127.0.0.1",port=3306,user="root",password=MyPassWord,database="tempdb")
 		cursor = db.cursor()
 		print(username,msg,gentime)
 		cursor.execute(store_sql,(username,msg,gentime))
@@ -96,7 +102,7 @@ def delete_msg(request):
 	gentime = gentime.replace('T',' ')
 	gentime = gentime.replace('Z','000')
 	print(username,msg,gentime)
-	db = pymysql.connect(host="127.0.0.1",port=3306,user="root",password="011119",database="tempdb")
+	db = pymysql.connect(host="127.0.0.1",port=3306,user="root",password=MyPassWord,database="tempdb")
 	cursor = db.cursor()
 	cursor.execute(delete_sql,(username,msg,gentime))
 	db.commit()
@@ -107,7 +113,7 @@ def delete_msg(request):
 def update_password(request):
 	user_name = request.data.get("username")
 	pass_word = request.data.get("password")
-	db = pymysql.connect(host="127.0.0.1",port=3306,user="root",password="011119",database="tempdb")
+	db = pymysql.connect(host="127.0.0.1",port=3306,user="root",password=MyPassWord,database="tempdb")
 	cursor = db.cursor()
 	res = cursor.execute(update_sql,(user_name,pass_word,user_name))
 	res = {"role": "admin", "code": 0, "msg": "修改密码成功", "username": user_name}
@@ -118,7 +124,7 @@ def add_chat_room(request):
 	admin_name = request.data.get("admin_name")
 	room_name = request.data.get("room_name")
 	gen_time = request.data.get("gen_time")
-	db = pymysql.connect(host="127.0.0.1",port=3306,user="root",password="xyf010830",database="tempdb")
+	db = pymysql.connect(host="127.0.0.1",port=3306,user="root",password=MyPassWord,database="tempdb")
 	cursor = db.cursor()
 	res = cursor.execute(add_chat_room_sql,(admin_name,room_name, gen_time))
 	res = {"role": "admin", "code": 0, "msg": "添加聊天室成功", "username": admin_name}
@@ -128,7 +134,7 @@ def add_chat_room(request):
 def join_room(request):
 	user_name = request.data.get("user_name")
 	room_name = request.data.get("room_name")
-	db = pymysql.connect(host="127.0.0.1",port=3306,user="root",password="xyf010830",database="tempdb")
+	db = pymysql.connect(host="127.0.0.1",port=3306,user="root",password=MyPassWord,database="tempdb")
 	cursor = db.cursor()
 	res = cursor.execute(update_sql,(user_name,room_name))
 	res = {"role": "admin", "code": 0, "msg": "加入聊天室成功", "username": user_name}
@@ -139,7 +145,7 @@ def add_dynamic(request):
 	user_name = request.data.get("user_name")
 	content = request.data.get("content")
 	gen_time = request.data.get("gen_time")
-	db = pymysql.connect(host="127.0.0.1",port=3306,user="root",password="xyf010830",database="tempdb")
+	db = pymysql.connect(host="127.0.0.1",port=3306,user="root",password=MyPassWord,database="tempdb")
 	cursor = db.cursor()
 	res = cursor.execute(add_dynamic_sql,(user_name,content,gen_time))
 	res = {"role": "admin", "code": 0, "msg": "添加动态成功", "username": user_name}
@@ -151,7 +157,7 @@ def add_comment(request):
 	dynamic_id = request.data.get("dynamic_id")
 	content = request.data.get("content")
 	gen_time = request.data.get("gen_time")
-	db = pymysql.connect(host="127.0.0.1",port=3306,user="root",password="xyf010830",database="tempdb")
+	db = pymysql.connect(host="127.0.0.1",port=3306,user="root",password=MyPassWord,database="tempdb")
 	cursor = db.cursor()
 	res = cursor.execute(add_comment_sql,(user_name,dynamic_id,content,gen_time))
 	res = {"role": "admin", "code": 0, "msg": "添加评论成功", "username": user_name}
