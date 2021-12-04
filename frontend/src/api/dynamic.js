876c17e6258d46234b50f8c338a317413b4b38df
xuyuanfan <120812312@qq.com>
2021-12-04 19:42:17 +0800

    modify dynamic.js

--- frontend/src/api/dynamic.js
+++ frontend/src/api/dynamic.js
@@ -1,17 +1,16 @@
 import request from '@/utils/request'
 
-export function adddynamic (data) {
-  return request({
-    url: '/api/add_dynamic/',
-    method: 'post',
-    data
-  })
+export function addDynamic (data) {
+  return request({
+    url: '/api/add_dynamic/',
+    method: 'post',
+    data
+  })
 }
 
-export function getdynamic () {
-    return request({
-      url: '/api/get_dynamic/',
-      method: 'post',
-      data
-    })
+export function getHistoryDynamic () {
+  return request({
+    url: '/api/history_dynamic/',
+    method: 'post'
+  })
 }
